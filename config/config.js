9da9baaaa257a67e292fc89431558519fd213be3
j-steinhaus <jessicamichele11@gmail.com>
2022-10-11 20:14:02 -0600

    created package.json

--- config/config.js
+++ config/config.js
@@ -1,15 +1,22 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
-require('dotenv').config();
+let sequelize;
 
-// connection to db 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    });
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
+// connectin to db 
 
 module.exports = sequelize;
-
